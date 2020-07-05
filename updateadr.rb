3a373667f3ab2a7ad82c4911bb6c2d2afcba1351
Biplav Tialsina <biplav.timalsina@imail2.org>
2020-07-05 16:38:09 -0700

    add formula

---
+++ updateadr.rb
@@ -0,0 +1,12 @@
+class Updateadr < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/biplavtimalsina/updateADR/archive/v1.0.0.tar.gz"
+  sha256 "fb25c4c70597be31055177e7ba9553a67a5b88fdbf21033a98b45f515c470d09"
+  license ""
+
+  def install
+      bin.install "updateadr"
+  end
+
+end
